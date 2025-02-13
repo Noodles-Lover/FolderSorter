322560766e9faab0eda383fa837500c067fc0c30
Noodles <yummynoodles25@gmail.com>
2025-02-13 17:17:00 +0800

    v1.0
    增加设置、说明和编号

--- js/format.js
+++ js/format.js
@@ -18,12 +18,45 @@ function formatDuration(seconds) {
 	return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
 }
 
+const units = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
 function formatFileSize(size) {
-	const units = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
 	let i = 0;
 	while (size >= 1024 && i < units.length - 1) {
 		size /= 1024;
 		i++;
 	}
 	return `${size.toFixed(2)} ${units[i]}`;
+}
+
+// 带单位的文件大小 还原为纯数字
+function parseFileSize(fileSizeStr) {
+	const [sizeStr, unit] = fileSizeStr.split(' ');
+	const size = parseFloat(sizeStr);
+	const unitIndex = units.indexOf(unit);
+	let originalSize = size;
+
+	for (let i = unitIndex; i > 0; i--) {
+		originalSize *= 1024;
+	}
+
+	return originalSize;
+}
+
+// 时长 还原为纯数字
+function parseDuration(durationStr) {
+    if (durationStr === '--') {
+        return -1; // 特殊值 -1
+    }
+
+    // 检查是否以 "秒" 结尾
+    if (durationStr.endsWith('秒')) {
+        return parseInt(durationStr, 10); // 直接解析为整数
+    }
+
+    // 分割小时、分钟和秒
+    const [hours, minutes, seconds] = durationStr.split(':').map(Number);
+
+    // 计算总秒数
+    return hours * 3600 + minutes * 60 + seconds;
 }
--- js/support.js
+++ js/support.js
@@ -33,7 +33,7 @@ async function getMediaDuration(file) {
 async function verifyPermission(fileHandle) {
 	const options = {};
 	options.mode = 'readwrite';
-	
+
 	// Check if permission was already granted. If so, return true.
 	if ((await fileHandle.queryPermission(options)) === 'granted') {
 		return true;
@@ -45,3 +45,49 @@ async function verifyPermission(fileHandle) {
 	// The user didn't grant permission, so return false.
 	return false;
 }
+
+async function openFolder(folderHandle, isRecursion, ignoreOtherFile) {
+	let folderFiles = []
+
+	for await (const fileHandle of folderHandle.values()) {
+		// 用fileHandle遍历文件夹内的文件（FileSystemFileHandle类）
+		console.log(`Entry name: ${fileHandle.name}, Entry kind: ${fileHandle.kind}`);
+
+		if (fileHandle.kind === 'file') { // 处理文件
+			const file = await fileHandle.getFile();
+
+			if (ignoreOtherFile) {
+				// 如果不是三种媒体文件，直接返回
+				if (!(file.type.startsWith('image') || file.type.startsWith('audio') || file.type.startsWith(
+						'video') || file.type.endsWith('.mov'))) continue
+			}
+
+			getMediaDuration(file).then(duration => {
+				folderFiles.push({
+					fileHandle: fileHandle,
+					name: file.name,
+					size: formatFileSize(file.size),
+					length: formatDuration(duration),
+					lastModified: new Date(file.lastModified)
+						.toLocaleString()
+				})
+				// console.log('媒体长度为：', duration, '秒');
+			}).catch(error => {
+				// this.$Message.error('获取媒体长度失败');
+				console.error('获取媒体长度失败:', error);
+			});
+
+		} else if (fileHandle.kind === 'directory') {
+			// 处理子目录
+			if (isRecursion) {
+				let subFiles = await openFolder(fileHandle, true, ignoreOtherFile)
+				folderFiles = folderFiles.concat(subFiles)
+			}
+		}
+	}
+	return folderFiles
+}
+
+// function getSumSize(folderHandle){
+
+// }
